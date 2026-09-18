da753ccc5110854a34f432340cc591570533dfe1
wlkllf <wlkllf@189.cn>
2026-09-18 10:13:05 +0800

    Create DeviceMacInterceptor.kt

---
+++ app/src/main/java/top/yogiczy/mytv/utils/DeviceMacInterceptor.kt
@@ -0,0 +1,28 @@
+package top.yogiczy.mytv.utils
+
+import android.content.Context
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import top.yogiczy.mytv.utils.MacUtil
+
+class DeviceMacInterceptor(private val appContext: Context) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+
+        val mac = MacUtil.getEthernetMac()
+        val appInstanceId = MacUtil.getAppInstanceId(appContext)
+
+        val builder: Request.Builder = originalRequest.newBuilder()
+        if (mac.isNotEmpty()) {
+            builder.header("X-Device-Mac", mac)
+        }
+        if (appInstanceId.isNotEmpty()) {
+            builder.header("X-App-InstanceId", appInstanceId)
+        }
+
+        val newRequest = builder.build()
+        return chain.proceed(newRequest)
+    }
+}
+
