0916d15daba6be40a655cc7de03b7d479b4d7cab
wlkllf <wlkllf@189.cn>
2026-09-23 18:16:30 +0800

    Update MyTVApplication.kt

--- app/src/main/java/top/yogiczy/mytv/MyTVApplication.kt
+++ app/src/main/java/top/yogiczy/mytv/MyTVApplication.kt
@@ -1,19 +1,20 @@
 package top.yogiczy.mytv
 
 import android.app.Application
+import okhttp3.OkHttpClient
 import top.yogiczy.mytv.utils.DeviceMacInterceptor
 
 class MyTVApplication : Application() {
     companion object {
         lateinit var instance: MyTVApplication
-        lateinit var okHttpClient: okhttp3.OkHttpClient
+        lateinit var okHttpClient: OkHttpClient
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
 
-        okHttpClient = okhttp3.OkHttpClient.Builder()
+        okHttpClient = OkHttpClient.Builder()
             .addInterceptor(DeviceMacInterceptor(this))
             .build()
     }
