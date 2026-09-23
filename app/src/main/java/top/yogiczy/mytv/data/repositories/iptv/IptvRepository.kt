ec59e4596b76fb5ef140d650340502ea80e5427a
wlkllf <wlkllf@189.cn>
2026-09-23 22:10:57 +0800

    Update IptvRepository.kt

--- app/src/main/java/top/yogiczy/mytv/data/repositories/iptv/IptvRepository.kt
+++ app/src/main/java/top/yogiczy/mytv/data/repositories/iptv/IptvRepository.kt
@@ -2,8 +2,8 @@ package top.yogiczy.mytv.data.repositories.iptv
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
 import okhttp3.Request
-import top.yogiczy.mytv.MyTVApplication
 import top.yogiczy.mytv.data.entities.Iptv
 import top.yogiczy.mytv.data.entities.IptvGroup
 import top.yogiczy.mytv.data.entities.IptvGroupList
@@ -11,14 +11,24 @@ import top.yogiczy.mytv.data.entities.IptvList
 import top.yogiczy.mytv.data.repositories.FileCacheRepository
 import top.yogiczy.mytv.data.repositories.iptv.parser.IptvParser
 import top.yogiczy.mytv.utils.Logger
+import top.yogiczy.mytv.utils.DeviceMacInterceptor
+import top.yogiczy.mytv.MyTVApplication
 
+/**
+ * 直播源获取
+ */
 class IptvRepository : FileCacheRepository("iptv.txt") {
     private val log = Logger.create(javaClass.simpleName)
 
+    /**
+     * 获取远程直播源数据
+     */
     private suspend fun fetchSource(sourceUrl: String) = withContext(Dispatchers.IO) {
         log.d("获取远程直播源: $sourceUrl")
 
-        val client = MyTVApplication.okHttpClient
+        val client = OkHttpClient.Builder()
+            .addInterceptor(DeviceMacInterceptor(MyTVApplication.application))
+            .build()
         val request = Request.Builder().url(sourceUrl).build()
 
         try {
@@ -26,6 +36,7 @@ class IptvRepository : FileCacheRepository("iptv.txt") {
                 if (!isSuccessful) {
                     throw Exception("获取远程直播源失败: $code")
                 }
+
                 return@with body!!.string()
             }
         } catch (ex: Exception) {
@@ -34,12 +45,18 @@ class IptvRepository : FileCacheRepository("iptv.txt") {
         }
     }
 
+    /**
+     * 简化规则
+     */
     private fun simplifyTest(group: IptvGroup, iptv: Iptv): Boolean {
         return iptv.name.lowercase().startsWith("cctv") || iptv.name.endsWith("卫视")
     }
 
+    /**
+     * 获取直播源分组列表
+     */
     suspend fun getIptvGroupList(
-        sourceUrl: String,
+        sourceUrl: String = "http://ys.lileifeng.top/mytvtgyy/getlist.php",
         cacheTime: Long,
         simplify: Boolean = false,
     ): IptvGroupList {
@@ -55,13 +72,13 @@ class IptvRepository : FileCacheRepository("iptv.txt") {
             if (simplify) {
                 return IptvGroupList(groupList.map { group ->
                     IptvGroup(
-                        name = group.name,
-                        iptvList = IptvList(group.iptvList.filter { iptv ->
+                        name = group.name, iptvList = IptvList(group.iptvList.filter { iptv ->
                             simplifyTest(group, iptv)
                         })
                     )
                 }.filter { it.iptvList.isNotEmpty() })
             }
+
             return groupList
         } catch (ex: Exception) {
             log.e("获取直播源失败", ex)
